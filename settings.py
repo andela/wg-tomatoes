448b79f639afa9d5c3544184121521b71bd777e4
gatana <sgatana@gmail.com>
2018-01-25 16:34:01 +0300

    [Feature] Add whitenoise to middleware classes

--- settings.py
+++ settings.py
@@ -24,13 +24,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-MIDDLEWARE_CLASSES = (
-    # Simplified static file serving.
-    # https://warehouse.python.org/project/whitenoise/
-    'whitenoise.middleware.WhiteNoiseMiddleware'
-)
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # configure postgres database
 DATABASES = {}
